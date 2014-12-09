28994e49d62a3d734b1718703f8ff06a96800b50
lynnug <lynnasiimwe@gmail.com>
2014-12-09 16:39:42 +0300

    fixed bulk message object vumi

--- go/apps/bulk_message_http_api/resource.py
+++ go/apps/bulk_message_http_api/resource.py
@@ -134,7 +134,7 @@ class MessageResource(BaseResource):
     def handle_PUT(self, request):
         try:
         	log.warning("vumi request")
-        	log.warning(request.content)
+        	log.warning(request.content.read())
         	payload = json.loads(request.content.read())
         except ValueError:
             self.client_error_response(request, 'Invalid Message for Vumi')
