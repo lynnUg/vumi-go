92e76084d73102d082ba7cb23a9c78cf10e8ffaf
lynnug <lynnasiimwe@gmail.com>
2014-12-09 16:22:00 +0300

    solving invalid message problem

--- go/apps/bulk_message_http_api/resource.py
+++ go/apps/bulk_message_http_api/resource.py
@@ -133,6 +133,8 @@ class MessageResource(BaseResource):
     @inlineCallbacks
     def handle_PUT(self, request):
         try:
+        	log.warning("vumi request")
+        	log.warning(request)
             payload = json.loads(request.content.read())
         except ValueError:
             self.client_error_response(request, 'Invalid Message')
