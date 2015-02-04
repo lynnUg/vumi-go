59e4cd13aa024dae05ffe4629a44d3dbdf770102
lynnug <lynnasiimwe@gmail.com>
2015-02-04 16:37:53 +0300

    adding create voucher

--- go/apps/access_mobile_http_api/vumi_app.py
+++ go/apps/access_mobile_http_api/vumi_app.py
@@ -15,7 +15,7 @@ from go.apps.access_mobile_http_api.resource import ApiResource
 from go.base.utils import extract_auth_from_url
 from go.vumitools.app_worker import GoApplicationWorker
 from vumi.components.window_manager import WindowManager
-
+from voucherapp import models
 
 
 
@@ -258,8 +258,11 @@ class AmHTTPWorker(GoApplicationWorker):
             convkey = data['convkey']
             usertoken=data['usertoken']
             accesstoken=data['accesstoken']
-            print "here is data"
-            print data
+            if "create_voucher" in data:
+                if data["create_voucher"]:
+                    voucher=Voucher.object.create("phone_number"=data['to_addr'])
+                    content= content+" "+voucher.voucher_number
+
             auth_headers = {
                 'Authorization': ['Basic %s' % (base64.b64encode(usertoken+':'+accesstoken),)],
                 }
