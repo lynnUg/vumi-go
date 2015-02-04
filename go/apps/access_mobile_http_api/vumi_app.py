34fecf748acd216697278a876c86a753f494fc1f
lynnug <lynnasiimwe@gmail.com>
2015-02-04 17:15:14 +0300

    small fix

--- go/apps/access_mobile_http_api/vumi_app.py
+++ go/apps/access_mobile_http_api/vumi_app.py
@@ -16,8 +16,7 @@ from go.base.utils import extract_auth_from_url
 from go.vumitools.app_worker import GoApplicationWorker
 from vumi.components.window_manager import WindowManager
 from go.voucherapp import models
-from django.core.management import setup_environ
-from go import settings
+from django.conf import settings
 
 
 
@@ -261,7 +260,8 @@ class AmHTTPWorker(GoApplicationWorker):
             accesstoken=data['accesstoken']
             if "create_voucher" in data:
                 if data["create_voucher"]:
-                    setup_environ(settings)
+                    if not settings.configured:
+                    settings.configure()
                     voucher=Voucher.object.create(phone_number=data['to_addr'])
                     content= content+" "+voucher.voucher_number
 
