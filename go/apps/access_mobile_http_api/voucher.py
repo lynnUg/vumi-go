c8acf61df2065bbf80efb1e74abea71dd7d5bfbd
lynnug <lynnasiimwe@gmail.com>
2015-02-24 12:24:55 +0300

    remove print

---
+++ go/apps/access_mobile_http_api/voucher.py
@@ -0,0 +1,66 @@
+import psycopg2
+import sys
+import random
+import string
+class Voucher:
+    def __init__(self,phone_number):
+        self.phone_number=phone_number
+        self.voucher_number=self.create_voucher_number()
+    def save(self):
+        con=None
+        try:
+            con= psycopg2.connect(database='vumi_db',user='vumi_login',
+            password='asiimwe18',host='localhost',port=5432)
+            cursor = con.cursor()
+            query = """
+            INSERT INTO voucherapp_voucher
+                (phone_number,voucher_number,created_at)
+            VALUES
+                (%(phone_number)s, %(voucher_number)s, now())
+            RETURNING id, phone_number, voucher_number
+            """ 
+            params = {
+            'phone_number': self.phone_number,
+            'voucher_number': self.voucher_number
+            }
+            cursor.execute(query, params)
+            con.commit()
+            #print "it worked"
+        except psycopg2.DatabaseError, e:
+            print 'Error %s' % e    
+            sys.exit(1)
+    
+    
+        finally:
+            if con:
+                con.close()
+    def get_voucher(self, voucher_number):
+        con=None
+        try:
+            con= psycopg2.connect(database='vumi_db',user='vumi_login',password='asiimwe18',host='localhost',port=5432)
+            query ="""
+                       SELECT id, phone_number, voucher_number
+                       FROM voucherapp_voucher
+                       WHERE voucher_number = %(voucher_number)s
+            """ 
+            params = {'voucher_number': voucher_number}
+            cursor = con.cursor()
+            cursor.execute(query, params)
+            result = cursor.fetchall()
+            if len(result) > 0:
+                return result[0]
+            else:
+                return None
+        except psycopg2.DatabaseError, e:
+            print 'Error %s' % e
+            sys.exit(1)
+        finally:
+            if con:
+                con.close()
+    def create_voucher_number(self):
+            length=7
+            while True:
+                voucher_number=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+                if not(self.get_voucher(voucher_number)):
+                    break
+            return voucher_number
